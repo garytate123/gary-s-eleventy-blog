8faa3fc00878966ce9b0adcaa42a47c24535316a
garytate123 <garytatelufc@gmail.com>
2021-03-08 15:22:17 +0000

    saved files

--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@
 // alert("hello");
 // console.log("hello, again");
 
-let fName = "Gary";
-let lName = "Tate";
-
-console.log(fName + " " + lName);
 
 let num1 = 5;
 let num2 = 10;
 
 console.log(num1 + num2);
 console.log(num1 * num2);
-console.log(num1 % num2);
+
+
+
+let fName = "Gary";
+let lName = "Tate";
+
+console.log(fName + " " + lName);
+
+
+
