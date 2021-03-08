6ad83f7b9c0ec1a03cdcdb54866525e9675f519e
garytate123 <garytatelufc@gmail.com>
2021-03-08 15:02:19 +0000

    added passThrougCopy in  eleventy config file, added js folder and main.js file, added a js.md post with script tag linking to main.js file.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+// alert("hello");
+// console.log("hello, again");
+
+let fName = "Gary";
+let lName = "Tate";
+
+console.log(fName + " " + lName);
+
+let num1 = 5;
+let num2 = 10;
+
+console.log(num1 + num2);
+console.log(num1 * num2);
+console.log(num1 % num2);
---
+++ posts/js.md
@@ -0,0 +1,11 @@
+---
+layout: layouts/post.njk
+title: JS
+description: "JS Test"
+tags:
+  - About Me
+date: 2021-03-04T22:17:38.976Z
+thumbnail: /images/uploads/gary-tate-.jpg
+---
+
+<script src="/js/main.js"></script>
